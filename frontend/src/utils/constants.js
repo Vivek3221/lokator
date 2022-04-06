fa14a85c0262c0f527476022bab4ff5ca5f6a105
karandevop <54063210+karandevop@users.noreply.github.com>
2022-04-06 20:33:35 +0530

    code

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,4 @@
-export const API_URL = window.location.href.includes('localhost')?"http://65.2.98.24:3000/api":"/api";
-export const  ItemperPage = 20
+export const API_URL = window.location.href.includes("localhost")
+  ? "http://3.87.191.94:3000//api"
+  : "/api";
+export const ItemperPage = 20;
