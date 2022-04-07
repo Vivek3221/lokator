876435c9de178f3230566cc5a4d492a0aa44100f
karandevop <54063210+karandevop@users.noreply.github.com>
2022-04-07 12:41:57 +0530

    build path updtge

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express    = require('express');
-const path       = require('path');
-require('dotenv').config()
-const db = require('./utils/connection')
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const db = require("./utils/connection");
+const cors = require("cors");
 const app = express();
 app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.use('/public/files',express.static("public/files"));
+app.use("/public/files", express.static("public/files"));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+  );
+  next();
+});
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    next();
- });
-
-app.get('/',(req, res)=>{
-   res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 // Database sync
 // db.sequelize.sync().then(() => {
@@ -30,47 +35,51 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cookieParser());
 app.use(function (err, req, res, next) {
-   console.log('asdasdasd');
- console.error(err)
-  res.status(500).send('Something broke!')
-})
+  console.log("asdasdasd");
+  console.error(err);
+  res.status(500).send("Something broke!");
+});
 
-let userRouter = require('./routes/user');
-let countryRouter = require('./routes/country');
-let machineRouter = require('./routes/machine');
-let productRouter = require('./routes/products');
-app.use('/api/user',userRouter);
-app.use('/api/country',countryRouter);
-app.use('/api/machine',machineRouter);
-app.use('/api/product',productRouter);
+let userRouter = require("./routes/user");
+let countryRouter = require("./routes/country");
+let machineRouter = require("./routes/machine");
+let productRouter = require("./routes/products");
+app.use("/api/user", userRouter);
+app.use("/api/country", countryRouter);
+app.use("/api/machine", machineRouter);
+app.use("/api/product", productRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../frontend/lokator/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "./frontend/build/index.html"));
 });
 app.use((req, res, next) => {
+  //res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+  );
+  const error = new Error("Route Not Found");
+  error.status = 404;
+  console.log(error);
+  next(error);
+});
 
-   //res.locals.message = err.message;
-   //res.locals.error = req.app.get('env') === 'development' ? err : {};
-res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-   const error = new Error('Route Not Found');
-   error.status=404;
-   console.log(error)
-   next(error);
- });
-
-
- // * Application-Level Middleware * //
- app.use((error, req, res, next) => {
-     res.status(error.status || 500);
-     res.send({
-     error : error.status,
-     message: error.message
-   });
- });
+// * Application-Level Middleware * //
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({
+    error: error.status,
+    message: error.message,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
- console.log(`Listing on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listing on port ${PORT}`);
 });
 module.exports = app;
