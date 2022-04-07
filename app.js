e23571c7b984b6da8ccff5ad58923b449e9b73cc
karandevop <54063210+karandevop@users.noreply.github.com>
2022-04-07 14:36:17 +0530

    c

--- app.js
+++ app.js
@@ -47,12 +47,12 @@ app.use("/api/user", userRouter);
 app.use("/api/country", countryRouter);
 app.use("/api/machine", machineRouter);
 app.use("/api/product", productRouter);
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-//   //file path added
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  //file path added
+});
 
 app.use((req, res, next) => {
   //res.locals.message = err.message;
