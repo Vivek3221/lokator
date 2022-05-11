42686f4282ea99f952ce4bb69777c930762f9a9e
Karan <54063210+karandevop@users.noreply.github.com>
2022-05-11 12:33:22 +0530

    code

--- frontend/src/components/NoData.js
+++ frontend/src/components/NoData.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function NoData(props) {
-  
   return (
     <div className="no-data">
+      <i class="fa fa-ban" aria-hidden="true"></i>
+
       <h3>No Data Found!</h3>
     </div>
-  )
+  );
 }
 
 export default NoData;
